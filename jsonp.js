080e9eecca3295932c4459c848510402eece7799
Alauddin Ansari <wonder_a7@yahoo.co.in>
2016-08-01 16:34:39 +0530

    Auto remove script tag
    Auto remove script tag after fetching data from server.

--- jsonp.js
+++ jsonp.js
@@ -8,22 +8,30 @@ var $jsonp = (function(){
       callback_name = options.callbackName || 'callback',
       on_success = options.onSuccess || function(){},
       on_timeout = options.onTimeout || function(){},
-      timeout = options.timeout || 10;
+      timeout = options.timeout || 10,
+		  uniqid = Math.ceil(Math.random()*1000);
 
     var timeout_trigger = window.setTimeout(function(){
       window[callback_name] = function(){};
       on_timeout();
+      document.getElementById('_id'+uniqid).remove();
     }, timeout * 1000);
 
     window[callback_name] = function(data){
       window.clearTimeout(timeout_trigger);
       on_success(data);
+      document.getElementById('_id'+uniqid).remove();
     };
+    
+    Element.prototype.remove = function() {
+			this.parentElement.removeChild(this);
+		}
 
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.async = true;
     script.src = src;
+    script.id = '_id'+uniqid;
 
     document.getElementsByTagName('head')[0].appendChild(script);
   };
